8a1d5a26185f4c72928cef40840fa1d60cdbd4fd
Xertis <xertis3785@gmail.com>
2024-11-20 23:22:37 +0300

    fix

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QApplication
 from src.app.aieditor import Aieditor
 
 
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = Aieditor()
     window.show()
     sys.exit(app.exec())
